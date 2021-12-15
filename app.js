d7fc02bff3e53ed8248ee26ef2505b351f91f63f
Arun Maurya <arunmaurya621@gmail.com>
2021-12-15 19:08:48 +0530

    ready

--- app.js
+++ app.js
@@ -2,27 +2,25 @@ const express=require('express');
 const path=require('path')
 const app=express();
 const mysql=require('mysql');
-// const { Agent } = require('http');
+const { Agent } = require('http');
 
-// app.use(express.static('client'))
-// app.use(express.urlencoded({extended:false}));
-// app.use(express.json());
-
-// app.get('/',(req,res)=>{
-    
-//     res.sendFile(path.join(__dirname,'client/index.html'))
-    
-// })
-
-// app.post('/auth',(req,res)=>{
-//     console.log(req.body);
-//     console.log(req.ip);
-//     console.log( req.headers['user-agent']);
-//     res.send({'status':200})
-// })
+app.use(express.static('client'))
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.send("ITs working");
+    
+    res.sendFile(path.join(__dirname,'client/index.html'))
+    
 })
+
+app.post('/auth',(req,res)=>{
+    console.log(req.body);
+    console.log(req.ip);
+    console.log( req.headers['user-agent']);
+    res.send({'status':200})
+})
+
+
 
 app.listen(process.env.PORT || 3131,console.log("Running on 3131"))
