4b2f5a41935ce67a4894a388117aa93368d05351
Arun Maurya <arunmaurya621@gmail.com>
2021-12-15 17:51:53 +0530

    tester01

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const express=require('express');
 const path=require('path')
 const app=express();
 const mysql=require('mysql');
-const { Agent } = require('http');
+const req = require('express/lib/request');
+// const { Agent } = require('http');
 
-app.use(express.static('client'))
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+// app.use(express.static('client'))
+// app.use(express.urlencoded({extended:false}));
+// app.use(express.json());
+
+// app.get('/',(req,res)=>{
+    
+//     res.sendFile(path.join(__dirname,'client/index.html'))
+    
+// })
+
+// app.post('/auth',(req,res)=>{
+//     console.log(req.body);
+//     console.log(req.ip);
+//     console.log( req.headers['user-agent']);
+//     res.send({'status':200})
+// })
 
 app.get('/',(req,res)=>{
-    
-    res.sendFile(path.join(__dirname,'client/index.html'))
-    
-})
-
-app.post('/auth',(req,res)=>{
-    console.log(req.body);
-    console.log(req.ip);
-    console.log( req.headers['user-agent']);
-    res.send({'status':200})
+    res.send("ITs working");
 })
 
 app.listen(3131,console.log("Running on 3131"))
